5f6ac70ccac04664af27ce9d4ec53730033efe39
Kerby Martino <kerbymart@gmail.com>
2015-01-28 02:22:26 +0800

    Merge branch 'master' of github.com:kerbymart/gwtuikit
    Conflicts:
    	src/test/java/com/getuikit/gwt/client/ComponentsTest.java
    	uikit-almost-flat/src/test/java/com/getuikit/gwt/client/UIKitAlmostFlatTest.java

--- uikit-almost-flat/src/test/java/com/getuikit/gwt/client/ComponentsTest.java
+++ uikit-almost-flat/src/test/java/com/getuikit/gwt/client/ComponentsTest.java
@@ -5,6 +5,7 @@ import com.google.gwt.core.client.Callback;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.ScriptInjector;
 import com.google.gwt.junit.client.GWTTestCase;
+import com.google.gwt.user.client.ui.Button;
 
 /**
  * Created by kerby on 1/26/15.
@@ -36,6 +37,30 @@ public class ComponentsTest extends GWTTestCase {
                     @Override
                     public void onSuccess(Void aVoid) {
                         UIKit.notification("test", UIKit.STATUS.SUCCESS, 1, UIKit.POSITION.TOP_RIGHT);
+                    }
+                }).setWindow(ScriptInjector.TOP_WINDOW).inject();
+            }
+        }).setWindow(ScriptInjector.TOP_WINDOW).inject();
+    }
+
+    public void testStickyComponent(){
+        String moduleBase = GWT.getModuleBaseURL();
+        ScriptInjector.fromUrl(GWT.getModuleBaseURL() + "js/jquery-2.1.3.min.js").setCallback(new Callback<Void, Exception>() {
+            @Override
+            public void onFailure(Exception e) {
+                fail();
+            }
+            @Override
+            public void onSuccess(Void aVoid) {
+                ScriptInjector.fromUrl(GWT.getModuleBaseURL() + "js/uikit.js").setCallback(new Callback<Void, Exception>() {
+                    @Override
+                    public void onFailure(Exception e) {
+                        fail();
+                    }
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Button button = new Button("Test");
+                        //UIKit.sticky(button.getElement());
                     }
                 }).setWindow(ScriptInjector.TOP_WINDOW).inject();
             }
